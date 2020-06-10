d41c76a2e10fe610d65003f12c97b63ac3222ca1
Benjamin Clauss <benjaminclauss@protonmail.com>
2020-06-09 23:11:05 -0400

    Add other fields to Team model and in view

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -2,7 +2,7 @@ var Team = require('../models/team');
 
 // Display list of all Teams.
 exports.team_list = function(req, res, next) {
-    Team.find({}, 'name')
+    Team.find()
         .exec(function (err, teams) {
             if (err) { return next(err); }
             //Successful, so render
--- models/team.js
+++ models/team.js
@@ -4,7 +4,14 @@ var Schema = mongoose.Schema;
 
 var TeamSchema = new Schema(
     {
+      fifa: {type: String, required: true},
       name: {type: String, required: true},
+      league: {type: String, required: true},
+      attack: { type: Number, min: 0, max: 100, required: true },
+      defense: { type: Number, min: 0, max: 100, required: true },
+      midfield: { type: Number, min: 0, max: 100, required: true },
+      overall: { type: Number, min: 0, max: 100, required: true },
+      rating: { type: Number, min: 0, max: 100, required: true },
     }
 );
 
